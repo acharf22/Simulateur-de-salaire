73f5e50f85a6d60c71ceee6d6c513578945cd124
acharf22 <105658939+acharf22@users.noreply.github.com>
2022-05-18 17:47:02 +0200

    Update Bonjour.java

--- Bonjour.java
+++ Bonjour.java
@@ -1,5 +1,5 @@
  package hi;
-//Pour faire reconnaître ma classe scanner j'importe un package dans lequel il y a la classe scanner	
+//Pour faire reconnaÃ®tre ma classe scanner j'importe un package dans lequel il y a la classe scanner	
 	import java.util.Scanner;
 	
 	public class Bonjour {	
@@ -11,25 +11,27 @@
 			
 			// Un objet sacanner qui permet de lire la saisie clavier
 			Scanner scanner = new Scanner (System.in);
-			//l'objet possède des popriétés pour une chaîne de caractères 
+			//l'objet possÃ¨de des popriÃ©tÃ©s pour une chaÃ®ne de caractÃ¨res 
 			String nom;
 			//Je demande a l'user de saisir son nom
-			System.out.println("Veuillez écrire votre nom llah yar7am Lwaliddin  !");
-			//Afficher la chaine qui va être saisie avec un mot de bienvenue
+			System.out.println("Veuillez Ã©crire votre nom llah yar7am Lwaliddin  !");
+			//Afficher la chaine qui va Ãªtre saisie avec un mot de bienvenue
 			nom = scanner.nextLine();
-			System.out.println("Bienvenue " + nom);
-			//
+			System.out.println("Bienvenue " + nom); // la variable nom est en La concatenation avec le message "Bienvenue"
 			
 			//Passons maintenat au mode de paiement 
-			System.out.println("Choisissez votre mode de paiement : Pour un mode de paiement hebdomadaire, tapez 1. Pour un mode de paiement mensuel tapez un autre numéro");
+			System.out.println("Choisissez votre mode de paiement : Pour un mode de paiement hebdomadaire, tapez 1. Pour un mode de paiement mensuel tapez un autre numÃ©ro");
+			
 			double mode;
 			mode = scanner.nextDouble();
 			
 			if (mode == 1 ) {
-				
-					System.out.println("Faîtes entrer le nombre d’heures travaillées par semaine, un nombre qui ne devrait pas dépqsser 50 ");
+				    // Je donne a l'utilisateur la main d'entrer son propre tarif
+					System.out.println("Quel est votre tarif de paiement par heure ? ");
+					tarif = scanner.nextDouble();
+					System.out.println("FaÃ®tes entrer le nombre dâ€™heures travaillÃ©es par semaine, un nombre qui ne devrait pas dÃ©pqsser 50 ");
 					nombreh = scanner.nextDouble();
-					tarif = 25;
+					
 					if (nombreh <= 40) {
 						System.out.println( "Votre salaire est de " + nombreh*tarif);
 				                    	}
@@ -38,18 +40,27 @@
 						System.out.println(  "Votre salaire est de " + (( 40 * tarif ) + (( nombreh - 40 ) * ( tarif * 1.5))) );
 						//salaire = ( 40 * tarif ) + (( nombreh - 40 ) * ( tarif * 1.5))
 			                                                  }
+							 else {
+								 System.out.println(  "Le nombre dâ€™heure total que vous avez saisi depasse 50 ");
+							 }
 			                   }
 			
 			else {
-				     System.out.println("Faîtes entrer le nombre d’heures travaillées par mois ! Attention : Le nombre d’heure total ne doit pas dépasser 200 heures par mois ");
+				// Je donne a l'utilisateur la main d'entrer son propre tarif
+				System.out.println("Quel est votre tarif de paiement par heure ? ");
+				tarif = scanner.nextDouble();     
+				System.out.println("FaÃ®tes entrer le nombre dâ€™heures travaillÃ©es par mois ! Attention : Le nombre dâ€™heure total ne doit pas dÃ©passer 200 heures par mois ");
 				     nombrem = scanner.nextDouble();
-				     tarif=20;
+				    
 				     if (nombrem <= 180) {
 				    	 System.out.println( "Votre salaire est de " + nombrem*tarif);
 				                         }
-				     else  if (nombrem > 180 && nombrem <= 200) {
-				    	 System.out.println(  "Votre salaire est de " + (( 180 * tarif ) + (( nombrem - 180 ) * ( tarif * 1.5 ))) ); 
+				     else  if (nombrem > 180 && nombrem <= 200) { // je calcule le salaire auquel j'ajoute le supplement
+				    	 System.out.println(  "Votre salaire est de " + (( 180 * tarif )  + (( nombrem - 180 ) * ( tarif * 1.5 ))) ); 
 				                                              }
+				     else {
+				    	 System.out.println(  "Le nombre dâ€™heure total que vous avez saisi depasse 200 ");
+				     }
 					
 			}
 			}	
